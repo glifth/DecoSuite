89e4e1ec146682cc886aea4100a25ce0759ee4bd
glifth <lucas.rangeard@epita.fr>
2018-08-18 02:27:46 +0200

    print result in file

--- src/decoSuite.py
+++ src/decoSuite.py
@@ -1,49 +1,69 @@
-import color as color
+import os
 
-from Misc.fact import *
+from src.helper import *
+from tests.fact import *
 
-
-def test_entry(files):
-    file = open(files, 'r')
-    lst = []
-    for i in file:
-        if is_int(i):
-            lst.append(1)
-        else:
-            lst.append(0)
-    file.close()
-    return lst
+init_log_file = False
 
 
-def get_files_name(f):
-    l = []
-    with open(f) as names:
-        for name in names:
-            l.append(name[:len(name)-1])
-    return l
+def decoSuite(function):
+    def wrapper(*args, **kwargs):
+        test_list = []
+        result_list = []
 
+        with open("../tests/test_" + function.__name__ + ".dcs") as f:
+            name = os.path.basename(f.name)
+            for test in f:
+                if test[len(test) - 1] == '\n':
+                    test = test[:-1]
+                test_list.append(test)
+                if is_int(test):
+                    result_list.append(str(function(int(test))) + '\n')
+                else:
+                    result_list.append(function(test))
 
-def genere_test(name, f):
-    lst = get_files_name(name)
-    entete = "      "
-    for i in range(0, len(lst)):
-        entete += ("{:>" + str(len(lst[i])) + "}  ").format(i)
+        open_status = "a"
+        with open("../tests/log.txt", open_status) as log:
+            log.write("\n" + "{:-^80}".format(" " + name + " ") + '\n')
+            log.write("Test File Input \n")
+            if len(test_list) == 0:
+                log.write("No test in the file.\n")
+                return
+            longest = sorted(map(len, test_list))[len(result_list) - 1]
+
+            for i in range(0, len(result_list)):
+                log.write("  " + ("{:" + str(longest) + "}").format((test_list[i])) + "  =>  " + result_list[i])
+
+        return
+
+    return wrapper
+
+"""
+def genere_test(lst, f):
+    entete = "            "
+
+    for i in lst:
+        entete += ("{:>" + str(len(i)) + "}  ").format(i)
     f.write(entete + '\n')
 
-    f.write("-" * sum([len(i) for i in lst]) + 6 * "-" + '\n')
+    sep = "-" * len(entete) + '\n'
+    f.write(sep)
 
     for i in range(0, len(lst)):
         ligne = "{:>10} |".format(lst[i])
-        for j in range(1, len(lst)):
-            ligne += "{:3}  ".format(color.green("  OK"))
+        for j in range(0, len(lst)):
+            ligne += ("{:>" + str(len(lst[i])) + "}  ").format(color.green("  OK"))
         f.write(ligne + "\n")
 
-    f.write("-" * ((len(lst)) * 7) + '\n')
+    f.write(sep)
     f.write("{:>10} |\n".format('ratio'))
 
 
-name = "name_tests.txt"
+name = sorted(os.listdir("../tests"))
+
+print(name)
 
 with open("file.txt", 'w') as f:
     genere_test(name, f)
 
+"""
